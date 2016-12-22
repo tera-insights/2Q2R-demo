ba63493de92ab3bb8c3ac1957979405a950b972d
Jess Smith <smith.jessk@gmail.com>
2016-12-22 15:30:30 -0500

    stresstest: Asynchronously create users and clean up code

--- src/stresstest/run.ts
+++ src/stresstest/run.ts
@@ -1,8 +1,62 @@
 import * as config from 'config';
-import User from "./User";
+import * as softU2F from "soft-u2f";
 
-const numUsers = 5;
+import * as httputil from "../app/routes/2Q2R-server"
 
-for (let i = 0; i < numUsers; i++) {
-    new User(config.get("appID") as string, "user-" + i, config.get("2FAserver") as string)
+const device = softU2F.createDevice()
+const baseURL = config.get("2FAserver") as string;
+const users: Array<PromiseLike<any>> = [];
+
+for (let i = 0; i < 100; i++) {
+    const userID = "user-" + i;
+    users.push(httputil.get("/v1/register/request/" + userID)
+        .then((r: registerSetupReply) => {
+            httputil.post("/v1/register/wait", {
+                requestID: r.id,
+            }).then(() => {
+                console.log("Registration complete")
+            }).catch((e: Error) => {
+                throw e
+            })
+
+            httputil.post("/v1/register/challenge", {
+                requestID: r.id,
+            }).then((r: challengeReply) => {
+                device.register(baseURL, r.challenge, baseURL, userID).then((result) => {
+                    httputil.post("/v1/register", {
+                        successful: true,
+                        Data: result.response,
+                    }).catch((e: any) => {
+                        throw e
+                    })
+                }).catch((e: any) => {
+                    throw e
+                })
+            })
+        }).catch((e: any) => {
+            throw new Error(`Got error: ${e}`)
+        }));
+}
+
+Promise.all(users)
+    .then(() => { console.log("Users created") })
+    .catch((e) => { throw e })
+
+interface IRegisterData {
+    clientData: string; // serialized client data JSON
+    registrationData: string; // websafe-base64-encoded U2F registration binary
+}
+
+interface RegistrationResult {
+    keyID: string,
+    response: IRegisterData
+}
+
+interface registerSetupReply {
+    id: string
+    registerUrl: string
+}
+
+interface challengeReply {
+    challenge: string;
 }

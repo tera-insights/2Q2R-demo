ba5903facb42e786bcaced65f92da67f96b287c1
Jess Smith <smith.jessk@gmail.com>
2016-12-22 16:39:49 -0500

    stresstest: Simplify test sequence

--- src/stresstest/run.ts
+++ src/stresstest/run.ts
@@ -5,42 +5,27 @@ import * as httputil from "../app/routes/2Q2R-server"
 
 const device = softU2F.createDevice()
 const baseURL = config.get("2FAserver") as string;
-const users: Array<PromiseLike<any>> = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 5; i++) {
     const userID = "user-" + i;
-    users.push(httputil.get("/v1/register/request/" + userID)
+    const p = httputil.get("/v1/register/request/" + userID)
         .then((r: registerSetupReply) => {
             httputil.post("/v1/register/wait", {
                 requestID: r.id,
-            }).then(() => {
-                console.log("Registration complete")
-            }).catch((e: Error) => {
-                throw e
-            })
+            }).then(() => { console.log(`${userID} created`) }).timeout(15000)
 
-            httputil.post("/v1/register/challenge", {
+            return httputil.post("/v1/register/challenge", {
                 requestID: r.id,
-            }).then((r: challengeReply) => {
-                device.register(baseURL, r.challenge, baseURL, userID).then((result) => {
-                    httputil.post("/v1/register", {
-                        successful: true,
-                        Data: result.response,
-                    }).catch((e: any) => {
-                        throw e
-                    })
-                }).catch((e: any) => {
-                    throw e
-                })
             })
-        }).catch((e: any) => {
-            throw new Error(`Got error: ${e}`)
-        }));
+        }).then((r: challengeReply) => {
+            return device.register(baseURL, r.challenge, baseURL, userID)
+        }).then((result) => {
+            return httputil.post("/v1/register", {
+                successful: true,
+                Data: result.response,
+            })
+        })
 }
-
-Promise.all(users)
-    .then(() => { console.log("Users created") })
-    .catch((e) => { throw e })
 
 interface IRegisterData {
     clientData: string; // serialized client data JSON
